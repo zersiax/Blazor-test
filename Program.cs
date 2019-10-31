7f051d4e73bbdf2a8684ed9684679e9296696696
Florian Beijers <Florian@perplex.local>
2019-10-31 11:24:56 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System.Globalization;
+namespace BlazorDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
